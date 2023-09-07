773bb23cec13a548edfca25ec88dc67c3c312f3b
chingling008 <chinglingmadness@gmail.com>
2023-09-07 07:46:24 -0700

    dd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
  * Description: 'the program's description'
  * new-line: describe the parameter
- * 
  * Return: Always 0 (Success)
  */
 int main(void)

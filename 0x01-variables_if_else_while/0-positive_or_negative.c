43babb27821e24915efd958dc3c2bff51f986d4a
chingling008 <chinglingmadness@gmail.com>
2023-09-08 04:24:34 -0700

    First if & else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+ int n  = rand()
+
+    if(n > 0) {
+         printf("%d is postive\n", n);
+    } 
+    else if (n = 0) {
+       printf("%d is zero\n", n);
+    }
+   else if (n < 0) {
+       printf("%d is negative\n", n);
+    }
+    return 0;
+}
